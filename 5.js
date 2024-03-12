605f772be591a93979d8bd49bea89b353f0df31a
mimika <150206116+nofilo@users.noreply.github.com>
2024-03-12 23:11:52 +0800

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,12 @@
+document.write("Hello World");
+
+function sayHello(){
+  let input=prompt("你的名字是什么");
+  alert("hello "+input+", have a nice day!");
+}
+
+function showTopic(){
+  let x =document.getElementById('demo');
+  x.style.fontSize="25px";
+  x.style.color="green";
+}
